72ebe8663d5375107c67abec42d18d89a5b5bedf
HIPgirl95 <hannah@dawne.com>
2024-07-08 13:42:44 -0400

    Part 4

--- js/scripts.js
+++ js/scripts.js
@@ -23,9 +23,11 @@ function newItem(){
     crossOutButton.append(document.createTextNode('X'));
     li.append(crossOutButton);
 
-    crossOutButton.on('click', deleteListItem)
+    crossOutButton.on('click', deleteListItem);
 
     function deleteListItem(){
         li.addClass('delete')
-    }
+    };
+
+    $('#list').sortable();
 }
