c4f9fa813a103536029c06d9472262d85edfdd8e
HIPgirl95 <hannah@dawne.com>
2024-07-08 11:43:05 -0400

    Part 1

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+//*Hannah Hogan -- 2024
+
+function newItem(){
+
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
+    if (inputValue === ''){
+        alert('You must write something!');
+    }
+    else {
+        $('#list').append(li);
+    }
+
+}
